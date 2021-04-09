77bf6bd4a323c258f6336b903c822fcd2b28c6ab
mattako <mattako66@gmail.com>
2021-04-09 16:22:50 -0700

    Simple upload form added

--- frontend/plants626/projects/admin/src/app/manage/upload/upload.component.ts
+++ frontend/plants626/projects/admin/src/app/manage/upload/upload.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.css']
+  styleUrls: ['./upload.component.css'],
 })
 export class UploadComponent implements OnInit {
+  fileList: FileList = null;
+  fileUrls: any = null;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onLoad(files: FileList) {
+    let reader = new FileReader();
+    this.fileList = files;
+    reader.readAsDataURL(files[0]);
+    reader.onload = () => {
+      this.fileUrls = reader.result;
+    }
   }
-
 }
