fe089802fa792072f8cc429059704fec0c2d4b26
MattAko <mattako66@gmail.com>
2021-05-30 20:38:21 -0700

    Visual updates for upload page adn tooltip component updated

--- frontend/plants626/src/app/shared/tooltip/tooltip.component.ts
+++ frontend/plants626/src/app/shared/tooltip/tooltip.component.ts
@@ -1,9 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, Input } from "@angular/core";
 
 @Component({
     selector: 'app-tooltip',
     templateUrl: './tooltip.component.html',
+    styleUrls: ['./tooltip.component.css'],
 })
 export class TooltipComponent{
+    @Input('msg') msg: string = '';
+    visible: boolean = false;
+
     constructor(){ }
+
+    mouseEnter(){
+        console.log('Hello')
+        this.visible = true;
+    }
+
+    mouseLeave(){
+        console.log('Bye')
+        this.visible = false;
+    }
 }
