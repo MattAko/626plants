bfebc60caa57e5ace2d448b7e62927d3c4c7e008
MattAko <mattako66@gmail.com>
2021-05-31 23:36:15 -0700

    Changes to home page

--- frontend/plants626/projects/admin/src/app/navbar/navbar.component.ts
+++ frontend/plants626/projects/admin/src/app/navbar/navbar.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 
 @Component({
